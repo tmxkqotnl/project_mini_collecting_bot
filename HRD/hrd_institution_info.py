bf6a2e7101db404a723553454952ae9a0d90f055
tmxkqotnl <tmxkqotnl@gmail.com>
2022-06-09 08:49:18 +0900

    feat, 훈련기관 정보를 얻기위한 py

---
+++ HRD/hrd_institution_info.py
@@ -0,0 +1,11 @@
+from typing import Union
+from bs4 import BeautifulSoup
+from bs4.element import Tag, NavigableString
+
+#################################################################################
+# tracseId=AIG20210000328968 # 훈련과정ID
+# tracseTme=2 # 회차
+# crseTracseSe=C0061 # 훈련종류 코드
+# trainstCstmrId=500020054508 # 훈련기관ID
+# pageId # 훈련과정 정보 - #undefined, 훈련기관 정보 None(비어있음)
+#################################################################################
