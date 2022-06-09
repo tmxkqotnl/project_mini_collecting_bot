5df2f2023b9f32a7c48036c3d58beb824e0ad1c9
tmxkqotnl <tmxkqotnl@gmail.com>
2022-06-09 17:05:13 +0900

    feat, check types & refac

--- HRD/api_process.py
+++ HRD/api_process.py
@@ -1,42 +1,22 @@
+from os import getenv
 from requests import Response, get, HTTPError, RequestException
 from bs4 import BeautifulSoup
 import lxml
-from common.const import HRD_OPEN_API_URLS
+from common.const import (
+    HEADERS,
+    HRD_OPEN_API_URLS,
+    INSTITUTION_SEARCH_DETAIL,
+    TRAINING_TYPE,
+)
+from common.libs import request_get
 
 from common.url import URL
 
 
-def request_api_response(opt: dict[str, str], request_url: str):
-    dict_has_key(opt, "authKey")
+def request_api_response(opt: dict[str, str], base_url: str):
+    opt["authKey"] = getenv("HRD_API_KEY")
 
-    url = URL(request_url)
-    url.set_params(opt)
-
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
-    }
-    res = get(url.get_full_url(), headers=HEADERS)
-    if res.status_code != 200:
-        raise HTTPError(
-            "Bad Reponse - {status_code}, {content}".format(
-                status_code=res.status_code, content=res.text
-            )
-        )
-
-    return parse_api_response_to_xml(res)
-
-
-def parse_api_response_to_xml(res: Response) -> BeautifulSoup:
-    xml = BeautifulSoup(markup=res.content, features="lxml-xml")
-
-    if xml.find("error"):
-        raise RequestException(
-            "response has a tag 'error' : {full_url}\n {error_message}".format(
-                full_url=res.url, error_message=xml.find("error").contents
-            )
-        )
-
-    return xml
+    return request_get(opt,base_url,parse_to='xml')
 
 
 def dict_has_key(d: dict[str, str], key: str):
@@ -50,4 +30,18 @@ def get_hrd_url(category: str, info_type: str):
     try:
         return HRD_OPEN_API_URLS[category][info_type]
     except KeyError:
-        raise KeyError("No case for info_type - {}".format(info_type))
+        raise KeyError(
+            "No case for category or info_type - {c}, {i}".format(
+                c=category, i=info_type
+            )
+        )
+
+
+def check_info_detail_type(info_detail: str):
+    if info_detail is None or info_detail not in INSTITUTION_SEARCH_DETAIL:
+        raise ValueError("info_detail must be one of {type}".format(type=info_detail))
+
+
+def check_info_type(info_type: str):
+    if info_type is None or info_type not in TRAINING_TYPE:
+        raise ValueError("info_detail must be one of {type}".format(type=info_type))
